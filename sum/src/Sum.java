7fd9da99005ec899b551c17501871bffb194f7a2
gkpgaurav06 <gaurav.nitkkr16.com>
2018-12-31 10:53:05 +0530

    added comment in Sum.java file

--- sum/src/Sum.java
+++ sum/src/Sum.java
@@ -16,14 +16,22 @@ import java.util.Scanner;
  * @author Gaurav Singh
  */
 public class Sum {
+
+    /* It is main function to read input from user and then find the sum of the input. */
     public static void main(String [] args){
+	System.out.println("Enter as many number as needed and end it with a");
         Scanner input = new Scanner(System.in);
-        System.out.println("Input an integer");
+
+	/*Reads the different input from the user. */
         int n;
+
+	/*Stores the sum of all the input number. */
         int sum = 0;
-        while ((n = input.nextInt()) > 0) {
+        while ((n = input.nextInt()) != a ) {
             sum = sum + n;
         }
+
+	/*Print the sum of the input number */
         System.out.println(sum + " is the sum of input no.");
         return;
     }
