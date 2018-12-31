fd498129792784832a986252f8c76a280513f037
gkpgaurav06 <gaurav.nitkkr16.com>
2018-12-31 10:56:01 +0530

    Corrected logic of TomJerry files to throw error if input number not in range

--- TomJerry/src/TomJerry.java
+++ TomJerry/src/TomJerry.java
@@ -1,7 +1,25 @@
+/*
+ * Gaurav Singh,
+ *
+ * Stack Route,
+ *
+ * Boeing India Pvt Ltd.
+ */
 import java.util.Scanner;
 
+/**
+ * The class allow the user to guess the number and suggest whether the guessed number is
+ * greater than or less then the supposed number.
+ *
+ * @version 1.10 31-Dec-2018
+ *
+ * @author Gaurav Singh
+ */
 public class TomJerry {
     public static void oddEven(int number) {
+	if(number < 30 && number > 20){
+	  System.out.println("Wrong input. Please enter between 30 and 20");
+	}
         if (number % 2 == 0) {
             if (number < 30 && number > 20) {
                 System.out.println("Jerry");
